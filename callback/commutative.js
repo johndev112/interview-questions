9ac4414ad5ec483550f341b4badb70da717c2a77
William Rogers <105190715+williamrogersdev@users.noreply.github.com>
2022-08-24 21:53:13 -0500

    Create commutative.js

---
+++ callback/commutative.js
@@ -0,0 +1,2 @@
+Challenge 17
+Create a function commutative that accepts two callbacks and a value. commutative will return a boolean indicating if the passing the value into the first function, and then passing the resulting output into the second function, yields the same output as the same operation with the order of the functions reversed (passing the value into the second function, and then passing the output into the first function).
