6900f0d3c08aa8f219bc25ffb4c7d209164aaff2
William Rogers <105190715+williamrogersdev@users.noreply.github.com>
2022-08-24 22:13:57 -0500

    Create objFilter.js

---
+++ objs/objFilter.js
@@ -0,0 +1,2 @@
+Challenge 18
+Create a function objFilter that accepts an object and a callback. objFilter should make a new object, and then iterate through the passed-in object, using each key as input for the callback. If the output from the callback is equal to the corresponding value, then that key-value pair is copied into the new object. objFilter will return this new object.
