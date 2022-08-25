f4f35e70f1d67fa548a55d86b3edcd09654334cd
William Rogers <105190715+williamrogersdev@users.noreply.github.com>
2022-08-24 22:48:39 -0500

    Create pipe.js

---
+++ reduce/pipe.js
@@ -0,0 +1,2 @@
+Challenge 20
+Create a function pipe that accepts an array (of functions) and a value. pipe should input the value into the first function in the array, and then use the output from that function as input for the second function, and then use the output from that function as input for the third function, and so forth, until we have an output from the last function in the array. pipe should return the final output.
