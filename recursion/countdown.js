49f6960f1f3f2404803401dbd2a1d3cf4ba1bec5
William Rogers <105190715+williamrogersdev@users.noreply.github.com>
2022-09-14 23:37:22 -0500

    Update countdown.js

--- recursion/countdown.js
+++ recursion/countdown.js
@@ -1,2 +1,17 @@
 Challenge 1
 Write a recursive function countdown that accepts a positive integer n as an input and logs every number from n (inclusive) to 0 (exclusive) to the console.
+
+
+// Challenge 1
+function countdown(n) {
+  
+  //base case if n <= 0
+  if (n <= 0) {
+    return n
+  }
+  
+  //recursive case 
+  //subtract 1 from n
+  console.log(n)
+return countdown(n - 1)
+}
