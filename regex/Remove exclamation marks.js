7fc511f6869478f9178a39805ff5bc2c3517b292
William Rogers <105190715+williamrogersdev@users.noreply.github.com>
2022-09-15 16:25:40 -0500

    Update Remove exclamation marks.js

--- regex/Remove exclamation marks.js
+++ regex/Remove exclamation marks.js
@@ -1,1 +1,11 @@
-Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+
+
+
+
+function removeExclamationMarks(s) {
+  //use .replace to replace all ! in the string and replace then with an empty string
+  //gi globally removes all instances of !
+  return s.replace(/!/gi, '');
+}
